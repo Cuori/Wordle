c017bec54a9513730ab7d8417e519949fe942fc1
Cuori <Teerapat.dan0@gmail.com>
2022-02-22 11:13:31 +0700

    Finished wordle clone

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
+from ast import While
 import numpy as np
 import random
 
 possible_words = np.loadtxt("possible_word.txt",dtype="str")
 word = random.choice(possible_words)
-test_word = "clone"
+
 
 def splitting(word):
     return [char for char in word]
 
-test_list = splitting("clone")
-guess_1 = splitting("crane")
-res_1 = [0,0,0,0,0]
-for i in range(5):
-    if test_list.count(guess_1[i]) >= 1:
-        res_1[i] = "yellow"
-        if test_list[i] == guess_1[i]:
-            res_1[i] = "green"
-    else:
-        res_1[i] = "black" 
+game = "on"
+guess = ""
+input_stat = True
+word_ = splitting(word)
+counter = 0
 
-print(guess_1)
-print(res_1)
+while(game == "on"):
+    while (input_stat == True):
+        guess = str(input("Type 5 letter word"))
+        guess_ = splitting(guess)
+        if guess in possible_words:
+            input_stat = False
+    res_1 = [0,0,0,0,0]
+    for i in range(5):
+        if word_.count(guess_[i]) >= 1:
+            res_1[i] = "yellow"
+            if word_[i] == guess_[i]:
+                res_1[i] = "green"
+        else:
+            res_1[i] = "black"
+    counter+= 1
+    print(f"Round {counter}")
+    print(guess_)
+    print(res_1)
+    if res_1.count("green") == 5:
+        print("Congratz")
+    guess = ""
+    if (counter >= 6):
+        game = "off"
+        print(f"You lose the word is {word}")
+    else:
+        input_stat = True
+
+
+    
+
 
